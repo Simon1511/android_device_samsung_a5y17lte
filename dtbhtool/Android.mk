d10c14dae221eb3e777577e22ab4ca50d9a889f2
Ethan Yonker <dees_troy@teamw.in>
2019-04-04 10:50:41 -0500

    Make dtbhtool utils specific to this device
    Change-Id: I1994febf43fb10a30e0f2d968a35e3737abcf91b

--- dtbhtool/Android.mk
+++ dtbhtool/Android.mk
@@ -5,29 +5,32 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dtbimg.c
 LOCAL_STATIC_LIBRARIES := libfdt
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libdtbimg
-LOCAL_MODULE := libdtbimg
+LOCAL_MODULE := libdtbimga5y17lte
 include $(BUILD_HOST_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := mkbootimg.c
-LOCAL_STATIC_LIBRARIES := libdtbimg libfdt libcrypto_static
+LOCAL_STATIC_LIBRARIES := libdtbimga5y17lte libfdt libcrypto_static
 
-LOCAL_MODULE := mkdtbhbootimg
+LOCAL_MODULE := mkdtbhbootimga5y17lte
+LOCAL_MODULE_STEM := mkdtbhbootimg
 
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
-LOCAL_MODULE := unpackdtbhbootimg
+LOCAL_MODULE := unpackdtbhbootimga5y17lte
+LOCAL_MODULE_STEM := unpackdtbhbootimg
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := mkdtbimg.c
-LOCAL_STATIC_LIBRARIES := libdtbimg libfdt
+LOCAL_STATIC_LIBRARIES := libdtbimga5y17lte libfdt
 
-LOCAL_MODULE := dtbhtoolExynos
+LOCAL_MODULE := dtbhtoolExynosa5y17lte
+LOCAL_MODULE_STEM := dtbhtoolExynos
 
 include $(BUILD_HOST_EXECUTABLE)
 
@@ -36,13 +39,13 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dtbimg.c
 LOCAL_STATIC_LIBRARIES := libfdt
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libdtbimg
-LOCAL_MODULE := libdtbimg
+LOCAL_MODULE := libdtbimga5y17lte
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := mkbootimg.c
-LOCAL_STATIC_LIBRARIES := libdtbimg libfdt libcrypto_static libcutils libc
-LOCAL_MODULE := utility_mkdtbhbootimg
+LOCAL_STATIC_LIBRARIES := libdtbimga5y17lte libfdt libcrypto_static libcutils libc
+LOCAL_MODULE := utility_mkdtbhbootimga5y17lte
 LOCAL_MODULE_STEM := mkdtbhbootimg
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
@@ -53,7 +56,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
 LOCAL_STATIC_LIBRARIES := libcutils libc
-LOCAL_MODULE := utility_unpackdtbhbootimg
+LOCAL_MODULE := utility_unpackdtbhbootimga5y17lte
 LOCAL_MODULE_STEM := unpackdtbhbootimg
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
@@ -63,7 +66,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackdtbhimg.c
-LOCAL_MODULE := unpackdtbhimg
+LOCAL_MODULE := unpackdtbhimga5y17lte
 include $(BUILD_HOST_EXECUTABLE)
 
 $(call dist-for-goals,dist_files,$(LOCAL_BUILT_MODULE))
